b8c1864f9b9da7b09b81f71e0848c712658df42e
asarmukh <ayansarmukhanovv@gmail.com>
2025-10-28 17:17:46 +0500

    merge drive and ride service branches

--- cmd/driver-service/main.go
+++ cmd/driver-service/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
 	"ride-hail/internal/driver/adapter/handlers"
 	"ride-hail/internal/driver/adapter/psql"
 	"ride-hail/internal/driver/app/usecase"
 	"ride-hail/internal/shared/config"
 	"ride-hail/internal/shared/db"
+	"ride-hail/internal/shared/util"
+	"syscall"
+	"time"
 
 	"github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	log := util.New()
+
+	log.Info("DriverService", "Starting service initialization...")
+
 	cfg, err := config.LoadConfig("config.yaml")
 	if err != nil {
-		panic(err)
+		log.Fatal("Config", err)
 	}
+	log.OK("Config", "Configuration loaded successfully")
 
 	var ch *amqp091.Channel
 	// conn, ch, err := mq.ConnectToRMQ(&cfg.RabbitMQ)
@@ -27,7 +38,11 @@ func main() {
 	// defer ch.Close()
 
 	database := db.ConnectToDB(&cfg.Database)
+	if database == nil {
+		log.Fatal("Database", err)
+	}
 	defer database.Close()
+	log.OK("Database", "Connected successfully")
 
 	repo := psql.NewRepo(database)
 	service := usecase.NewService(repo, ch)
@@ -36,13 +51,30 @@ func main() {
 	mux := handler.Router()
 
 	server := &http.Server{
-		Addr:    ":3001",
+		Addr:    ":" + "3001",
 		Handler: mux,
 	}
 
-	// go func() {
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
+	go func() {
+		log.OK("HTTP", "ride-service running on :3001")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Error("HTTP", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Warn("RideService", "Shutting down ride-service...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error("HTTP", err)
+	} else {
+		log.OK("HTTP", "Server stopped gracefully")
 	}
-	// }()
+	log.Info("RideService", "Shutdown complete")
 }
--- migrations/005_enable_postgis_and_location.up.sql
+++ migrations/005_enable_postgis_and_location.up.sql
@@ -6,8 +6,15 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 -- Add geography (location) column to coordinates
 ALTER TABLE coordinates ADD COLUMN location GEOGRAPHY(Point, 4326);
 
+ALTER TABLE location_history ADD COLUMN location GEOGRAPHY(Point, 4326);
+
 -- Backfill existing rows with Point geometry
 UPDATE coordinates
+SET location = ST_SetSRID(ST_MakePoint(longitude::double precision, latitude::double precision), 4326)
+WHERE location IS NULL;
+
+
+UPDATE location_history
 SET location = ST_SetSRID(ST_MakePoint(longitude::double precision, latitude::double precision), 4326)
 WHERE location IS NULL;
 
